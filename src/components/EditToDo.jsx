73bc88d2abd5234e610e3912cc143fee0234b168
Valentijn <v.f.vermeer@gmail.com>
2023-11-20 04:46:09 +0100

    added EditToDo

--- src/components/EditToDo.jsx
+++ src/components/EditToDo.jsx
@@ -2,23 +2,19 @@ import React from 'react'
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
- const ToDoForm = ({ toDos, setToDos }) => {
 
-    const [title, setTitle] = useState('');
+ const EditToDo = ({ toDos, editToDo, toDo, setToDos, updateToDo }) => {
+
+    const [edit, setEdit] = useState(toDo.title);
 
     const handleSubmit = e => {
         e.preventDefault();
-        if (title) {
-          const newToDo = { id: uuidv4(), title};
-          setToDos([newToDo, ...toDos]);
-          // e.target.reset(); this will not work!!!!!
-          // setBook({});
-          setTitle('');
+        if (edit) {updateToDo(toDo.id, edit);
         }
       };
 
-    const handleToDoChange = e => {
-        setTitle(e.target.value);
+    const handleToDoChangeEdit = e => {
+        setEdit(e.target.value);
       };
 
       const emptyInput = e => {
@@ -34,34 +30,26 @@ import { v4 as uuidv4 } from 'uuid';
       {/* <input type="date" name="date" id="date" onChange={handleChange} value={book.date} /> */}
       {/* <button className=''>Add To do</button> */}
 
-        <div className='w-2/4 container'>
-            <div className="flex col-span-full">
-                <label htmlFor="text"  className="add-to-dos text-m font-bold leading-6 text-gray-800 text-left ml-1">
-                    Add your to-dos:
-                </label>
-            </div>
-            <div className="relative mt-2 rounded-md shadow-sm">
+        <div className='container flex items-center gap-1'>
+            <div className="relative rounded-md shadow-sm center items-center">
                 <input
                 type="text"
                 name="title"
-                value={title}
+                value={edit}
                 //id="key"
-                onChange={handleToDoChange}
+                onChange={handleToDoChangeEdit}
                 className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-gold sm:text-sm sm:leading-6"
-                placeholder="Your to do..."
+                //placeholder="Your to do..."
                 autoComplete="off"
                 />
             </div>
 
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-                <button onClick={emptyInput} type="button" className="text-sm font-semibold leading-6 text-gray-900">
-                    Cancel
-                </button>
+            <div className="flex items-center justify-end gap-x-6">
                 <button
                 type="submit"
                 className="rounded-md bg-tailwind-grey px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-lighter-grey focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lighter-grey"
                 >
-                Save
+                Update
                 </button>
             </div>
         </div>
@@ -69,4 +57,4 @@ import { v4 as uuidv4 } from 'uuid';
   )
 }
 
-export default ToDoForm
+export default EditToDo
